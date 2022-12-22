c69357c8fc56a4927ba7bb1412abc4389b4b3de5
navintar <106975042+navintarBT@users.noreply.github.com>
2022-12-22 22:57:26 +0700

    first commit

---
+++ index.html
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <link rel="stylesheet" href="style.css">
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+        <script src="https://kit.fontawesome.com/92d70a2fd8.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="header">
+            <p class="logo">ຮ້ານF FOUR</p>
+            <div class="cart"><i class="fa-solid fa-cart-shopping"></i><p id="count">0</p></div>
+        </div>
+        <div class="container">
+            <div id="root"></div>
+            <div class="sidebar">
+                <div class="head"><p>ສັ່ງເລີຍ</p></div>
+                <div id="cartItem">ລາຍການສີນຄ້າສັ່ງຊື້</div>
+                <div class="foot">
+                    <h3>ຍອດລວມ</h3>
+                    <h2 id="total">0.000 ກີບ</h2>
+                    
+                </div>
+
+                
+                <button onclick="validate()">ສັ່ງເລີຍ</button>
+
+
+            </div>
+        </div>
+        <script src="script.js"></script>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,120 @@
+const product = [
+    {
+        id: 0,
+        image: 'image/oishi.png',
+        title: 'ໂອອີຊີ',
+        price: 10,
+    },
+    {
+        id: 1,
+        image: 'image/pepsi.png',
+        title: 'ເປບຊີ',
+        price: 5,
+    },
+    {
+        id: 2,
+        image: 'image/sting.png',
+        title: 'ສະຕີງ',
+        price: 7,
+    },
+    {
+        id: 3,
+        image: 'image/ກາເຟ.png',
+        title: 'ກາເຟຮ້ອນ',
+        price: 15,
+    },
+    {
+        id: 4,
+        image: 'image/s.png',
+        title: 'ຊາຂຽວ',
+        price: 21,
+    },
+    {
+        id: 5,
+        image: 'image/n.png',
+        title: 'ຊາໝາກນາວ',
+        price: 20,
+    }
+    
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2> ${price}.000 ກີບ</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>ກົດສັ່ງຊື້</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+function validate() {
+
+    var total = document.getElementById("cartItem").innerHTML;
+    if (total == "ລາຍການສີນຄ້າສັ່ງຊື້") {
+        alert("ກະລຸນາເລືອກອາຫານຂອງທ່ານ. ຂອບໃຈ");
+    } else 
+
+    setTimeout(function() {
+        swal({
+                title: "ສຳເລັດ",
+                text: "ຂອບໃຈ ທີ່ໃຊ້ບໍລິການ",
+                type: "success", 
+                timer: 2500, 
+                showConfirmButton: false 
+            }, function(){
+                window.location = "https://anoulukk.github.io/waiting-page/"; 
+                });
+        });
+        
+
+
+    
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "ລາຍການສີນຄ້າສັ່ງຊື້";
+        document.getElementById("total").innerHTML = "ກີບ "+0+".000";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "" + total + ".000 ກີບ";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>${price}.000 ກີບ</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
+    
+}
